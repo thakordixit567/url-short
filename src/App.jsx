609c76dedbc1bb262e4ad2b8a8c0e0b3ed6b93fa
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2025-09-15 13:28:15 +0530

    UPDATE!

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,47 @@
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './App.css'
-import Applayout from './layouts/Applayout'
-import Landingpage from './pages/Landingpage'
-import Dashboard from './pages/dashboard'
-import Auth from './pages/auth'
-import Link from './pages/Link'
-import Redirectlink from './pages/Redirectlink'
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Applayout from "./layouts/Applayout";
+import Landingpage from "./pages/Landingpage";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import Link from "./pages/Link";
+import Redirectlink from "./pages/Redirectlink";
+import UrlProvider from "./context";
 
 const router = createBrowserRouter([
   {
-    element: <Applayout/> ,
+    element: <Applayout />,
     children: [
       {
-        path: '/',
-        element: <Landingpage/>,
+        path: "/",
+        element: <Landingpage />,
       },
       {
-        path: '/dashboard',
-        element: <Dashboard/>,
+        path: "/dashboard",
+        element: <Dashboard />,
       },
       {
-        path: '/auth',
-        element: <Auth/>,
+        path: "/auth",
+        element: <Auth />,
       },
-       {
-        path: '/link/:id',
-        element: <Link/>,
+      {
+        path: "/link/:id",
+        element: <Link />,
       },
-       {
-        path: '/:id',
-        element: <Redirectlink/>,
+      {
+        path: "/:id",
+        element: <Redirectlink />,
       },
     ],
-  }
-])
+  },
+]);
 
 function App() {
-  
-
   return (
-    <RouterProvider router={router} />
-  )
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  );
 }
 
-export default App
+export default App;
--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,29 @@
-import { createContext } from 'react';
+/* eslint-disable react/prop-types */
 
+import {createContext, useContext, useEffect} from "react";
+import {getCurrentUser} from "./db/apiAuth";
+import useFetch from "./hooks/usefetch";
 
-const UrlContext = createContext
+const UrlContext = createContext();
+
+const UrlProvider = ({children}) => {
+  const {data: user, loading, fn: fetchUser} = useFetch(getCurrentUser);
+
+  const isAuthenticated = user?.role === "authenticated";
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <UrlContext.Provider value={{user, fetchUser, loading, isAuthenticated}}>
+      {children}
+    </UrlContext.Provider>
+  );
+};
+
+export const UrlState = () => {
+  return useContext(UrlContext);
+};
+
+export default UrlProvider;
